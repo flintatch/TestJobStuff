d843a44551c61f3aa41593a442c64578f96f5264
gvlad <gvladovsky@gmail.com>
2022-11-14 20:53:03 +0300

    Added site

--- src/test/java/Tests/TestFives.java
+++ src/test/java/Tests/TestFives.java
@@ -1,18 +1,23 @@
 package Tests;
 
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
-public class TestFives extends TestBase {
+public class TestFives {
 
     @Test
     @DisplayName("Проверка запуска ресурса")
     void TestFirst() {
-        step("открыть ресурс", () -> {
+        step("открыть ресурс ИНОтв", () -> {
             open("https://russian.rt.com");
+        });
+        step("открыть ссылку ИНО ТВ", () -> {
+            $(".link_foreign-press").click();
         });
     }
 }
