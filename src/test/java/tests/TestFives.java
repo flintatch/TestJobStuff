d13060c574398f4668c549d5cd38f176168a3ce4
gvlad <gvladovsky@gmail.com>
2022-11-22 19:53:18 +0300

    bot added

--- src/test/java/tests/TestFives.java
+++ src/test/java/tests/TestFives.java
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class TestFives {
+public class TestFives extends TestBase {
 
     @Test
     @DisplayName("Проверка запуска ресурса")
